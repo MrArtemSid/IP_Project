56e2f641c81441002f9c8ea3d2fa11e531b647ef
MrArtemSid <artem.sid.key@gmail.com>
2023-04-06 17:20:20 +0400

    Implement sideload function
    Based on:
    https://github.com/MoKee/flash/blob/master/src/App.vue
    https://github.com/MoKee/flash/blob/master/src/utils/readFile.js
    
    Co-authored-by: xingrz <hi@xingrz.me>

---
+++ scripts/readFile.js
@@ -0,0 +1,9 @@
+function readFile(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = (evt) => resolve(new Uint8Array(evt.target.result));
+    reader.onabort = () => resolve(null);
+    reader.onerror = (evt) => reject(evt);
+    reader.readAsArrayBuffer(file);
+  });
+}
