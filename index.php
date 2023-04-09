515d52a28ac46da798f0b9f0a600bf816dce20e5
DmitryKrylow <dima20038266@gmail.com>
2023-04-09 21:06:07 +0400

    content.html: restruct GUI (front)

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <body>
+<div id="container" class="container">
 <?php
 $page = "content.html";
 include_once ('menu.php');
 ?>
 <div class="content">
-<?php
-include ($page);
-?>
+    <?php
+    include ($page);
+    ?>
+</div>
 </div>
 </body>
